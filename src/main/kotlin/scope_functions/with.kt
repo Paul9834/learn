eb5708779f79f2471ada67173839a52343a01947
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2022-05-14 12:50:04 -0500

    Practica With

---
+++ src/main/kotlin/scope_functions/with.kt
@@ -0,0 +1,15 @@
+
+
+fun main(args: Array<String>) {
+
+    val listaColores = listOf("Azul","Rojo","Amarillo")
+
+    with(listaColores) {
+        println("Los colores son $this")
+        println("Hay numero de colores $size")
+    }
+
+
+
+
+}
